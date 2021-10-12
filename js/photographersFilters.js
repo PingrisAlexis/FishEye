0f46570ed10b5877fa55672bbea353c7aa3c0884
Alexis <pingrisalexis@gmail.com>
2021-10-12 13:50:53 +0200

    create photographers filter tags

--- js/photographersFilters.js
+++ js/photographersFilters.js
@@ -1,24 +1,35 @@
 
-const filters = document.querySelectorAll('.filter');
+const filters = document.querySelectorAll('.photographers-filter');
 
 filters.forEach(filter => {
 
-    filter.addEventListener('click', function() {
+    filter.onclick = () => {
 
         let selectedFilter = filter.getAttribute('data-filter');
-        let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
-        let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
+        let photographerCardsToHide = document.querySelectorAll(`.photographers-filter:not([data-filter='${selectedFilter}'])`);
+        let photographerCardsToShow = document.querySelectorAll(`[data-filter='${selectedFilter}']`);
 
 
-        itemsToHide.forEach(el => {
+        photographerCardsToHide.forEach(el => {
             el.parentNode.parentNode.classList.add('hide');
             el.parentNode.parentNode.classList.remove('show');
+            el.classList.remove("active")
+            console.log("222222")
+
         });
 
-        itemsToShow.forEach(el => {
+        photographerCardsToShow.forEach(el => {
+            if (el.parentNode.parentNode.classList == "active") {
+
+                photographerCardsToHide = [];
+                photographerCardsToShow = document.querySelectorAll('[data-filter]');
+                console.log("33333")
+
+            }
             el.parentNode.parentNode.classList.remove('hide');
             el.parentNode.parentNode.classList.add('show');
-        });
+            el.classList.add("active")
 
-    });
+        });
+    };
 });
