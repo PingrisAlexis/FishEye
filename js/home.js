ff9ea45a2d184b95679915a47dda20cb9e1df8e2
Alexis <pingrisalexis@gmail.com>
2021-10-12 13:50:33 +0200

    add photographers-filter

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ fetch("fisheye.json")
         loadPhotographers.map(photographer =>
 
             photographersContainer.innerHTML +=
-                `<article class="photographer-card projects">
+                `<article class="photographer-card">
                         <a href="html/photographer-page.html?id=${photographer.id}">
                             <img class="photographer-portrait" src="asset/media/PhotographersPortraits/${photographer.portrait}" alt="Photographer Portrait">
                             <h2>${photographer.name}</h2>
@@ -17,7 +17,7 @@ fetch("fisheye.json")
                         <p>${photographer.tagline}</p>
                         <p>${photographer.price}€/jour</p>
                         <ul >${photographer.tags.map(tag =>
-                            `<li class="project"  data-filter="${tag}">#${tag}
+                            `<li class="photographers-filter"  data-filter="${tag}">#${tag}
                                    <span class="sr-only">Filtre les artistes selon la catégorie ${tag}</span>
                             </li>`).join(" ")}
                         </ul> 
@@ -28,27 +28,3 @@ fetch("fisheye.json")
     .catch(err => {
         console.log(err);
     });
-
-const filters = document.querySelectorAll('.filter');
-
-filters.forEach(filter => {
-
-    filter.addEventListener('click', function() {
-
-        let selectedFilter = filter.getAttribute('data-filter');
-        let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
-        let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
-
-
-        itemsToHide.forEach(el => {
-            el.parentNode.parentNode.classList.add('hide');
-            el.parentNode.parentNode.classList.remove('show');
-        });
-
-        itemsToShow.forEach(el => {
-            el.parentNode.parentNode.classList.remove('hide');
-            el.parentNode.parentNode.classList.add('show');
-        });
-
-    });
-});
