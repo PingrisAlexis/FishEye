eb438aa20e58f58d49647cea3a667cba1d8f82b9
Alexis <pingrisalexis@gmail.com>
2021-11-02 11:36:31 +0100

    fix count pb

--- src/js/likes.js
+++ src/js/likes.js
@@ -6,13 +6,13 @@ document.body.addEventListener( 'click',  (btn) => {
     let likesBtn = btn.target;
     let likes = likesBtn.previousElementSibling;
     let selectedBtn = likesBtn.getAttribute("data-id");
-    let likesCount = parseInt(likes.innerText);
 
     if (likesBtn.className === "far fa-heart like-btn") {
 
         if(!selectedBtnIndex.includes(selectedBtn)) {
             selectedBtnIndex.push(selectedBtn)
             likesBtn.style.fontWeight = "bold";
+            let likesCount = parseInt(likes.innerText);
             likesCount += 1;
             likes.innerText = likesCount;
             likesPerPhotographer();
@@ -20,7 +20,7 @@ document.body.addEventListener( 'click',  (btn) => {
         else {
             selectedBtnIndex = selectedBtnIndex.filter(item => item !== selectedBtn)
             likesBtn.style.fontWeight = "400";
-            // let likesCount = parseInt(likes.innerText);
+            let likesCount = parseInt(likes.innerText);
             likesCount -= 1;
             likes.innerText = likesCount;
             likesPerPhotographer();
