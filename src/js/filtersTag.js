c4fb2a2341ac1fc62c9a472a161ec3cb2e2600b8
Alexis <pingrisalexis@gmail.com>
2021-11-02 11:35:23 +0100

    Keyboard navigation

--- src/js/filtersTag.js
+++ src/js/filtersTag.js
@@ -1,29 +1,33 @@
 const filters = document.querySelectorAll('.photographers-filter');
 
-filters.forEach(filter => {
-
-    filter.onclick = () => {
-
-        let selectedFilter = filter.getAttribute("data-filter");
-        console.log(selectedFilter)
-        let photographerCardsToHide = document.querySelectorAll(`.photographers-filter:not([data-filter='${selectedFilter}'])`);
-        let photographerCardsToShow = document.querySelectorAll(`[data-filter='${selectedFilter}']`);
-
-        if (selectedFilter === "all") {
-            photographerCardsToHide = [];
-            photographerCardsToShow = document.querySelectorAll('[data-filter]');
+filters.forEach((filter) => {
+    filter.addEventListener("click",() => {
+        filtersTag(filter);
+    });
+    filter.addEventListener("keydown", (event) => {
+        if (event.keyCode === 13) {
+            filtersTag(filter);
         }
-
-        photographerCardsToHide.forEach(el => {
-            el.parentNode.parentNode.classList.add("hide");
-            el.parentNode.parentNode.classList.remove("show");
-            // el.classList.remove("active");
-        });
-
-        photographerCardsToShow.forEach(el => {
-            el.parentNode.parentNode.classList.remove("hide");
-            el.parentNode.parentNode.classList.add("show");
-            // el.classList.add("active");
-        });
-    };
+    });
 });
+
+function filtersTag(filter) {
+    let selectedFilter = filter.getAttribute("data-filter");
+    let photographerCardsToHide = document.querySelectorAll(`.photographers-filter:not([data-filter='${selectedFilter}'])`);
+    let photographerCardsToShow = document.querySelectorAll(`[data-filter='${selectedFilter}']`);
+
+    if (selectedFilter === "all") {
+        photographerCardsToHide = [];
+        photographerCardsToShow = document.querySelectorAll('[data-filter]');
+    }
+
+    photographerCardsToHide.forEach(el => {
+        el.parentNode.parentNode.classList.add("hide");
+        el.parentNode.parentNode.classList.remove("show");
+    });
+
+    photographerCardsToShow.forEach(el => {
+        el.parentNode.parentNode.classList.remove("hide");
+        el.parentNode.parentNode.classList.add("show");
+    });
+}
