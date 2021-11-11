bb5199217fb0e44a7ea68022fc0b1355b46cd4a2
Alexis <pingrisalexis@gmail.com>
2021-11-11 11:14:35 +0100

    add filter by categories

--- src/js/photographerPageBuild.js
+++ src/js/photographerPageBuild.js
@@ -2,55 +2,32 @@ const photographersPageCardsContainer = document.querySelector(".photographer-pa
 const mediasPageContainer = document.querySelector(".media-cards-container");
 const urlSearch = new URLSearchParams(window.location.search).get("id");
 const lightboxModalContent = document.querySelector(".lightbox-modal-content");
+const sortByContainer = document.querySelector(".order-by-select");
 
-let mediaArraySort = [];
-let sortByContainer = document.querySelector(".order-by-select");
-let sortByOptions = sortByContainer.options[sortByContainer.selectedIndex].value
-
-
-
-fetch('../../fisheye.json')
+fetch("../../fisheye.json")
     .then(resp => resp.json())
     .then(data => {
         let loadPhotographers = data.photographers;
         let loadMedias = data.media;
+        let currentPhotographerMedia = loadMedias.filter((media) => media.photographerId === parseInt(urlSearch));
 
-// pushHTML =                 mediasPageContainer.innerHTML += new MediaCard(media).getMediaCard();
-
-        sortByContainer.addEventListener("click",(index) => {
-            if (sortByOptions === "Popularité")  {
-                mediaArraySort = loadMedias.sort((a, b) => { // SORT BY POPULARITY
-                    return b.likes - a.likes
-                })
-                console.log("popu")
-            }
-            else if (sortByOptions === "Date")  {
-                console.log("date")
-            }
-            else if (sortByOptions === "Titre")  {
-                console.log("titre")
-            }
-        });
-
-        loadMedias
-            .filter((media) => media.photographerId === parseInt(urlSearch))
-            .map((media) => {
-
-
-
-
-
-
-                mediasPageContainer.innerHTML += new MediaCard(media).getMediaCard();
-                lightboxModalContent.innerHTML += new MediaCard(media).getMediaLightbox();
+        //Automatic order medias by likes
+        sortBy(currentPhotographerMedia, "likes").forEach((media) => {
+            mediasPageContainer.innerHTML += new MediaCard(media).getMediaCard();
+            lightboxModalContent.innerHTML += new MediaCard(media).getMediaLightbox();
         })
 
-        const photographer = loadPhotographers.find((photographer) => photographer.id === parseInt(urlSearch));
-        // loadPhotographers.find((photographer) => {
-            if (photographer) {
-                photographersPageCardsContainer.innerHTML = new PhotographerCard(photographer).getPhotographerPageCard();
+        //Event handler on categories select
+        sortByContainer.addEventListener("change",(e) => {
+           handleSort(e.target.value, currentPhotographerMedia, mediasPageContainer, lightboxModalContent)
+        });
+
+        //Automatic photographer hydrate HTML
+        const currentPhotographer = loadPhotographers.find((photographer) => photographer.id === parseInt(urlSearch));
+            if (currentPhotographer) {
+                photographersPageCardsContainer.innerHTML = new PhotographerCard(currentPhotographer).getPhotographerPageCard();
             }
-        // })
+
     })
     .catch(err => {
         console.log(err);
