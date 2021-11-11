84ad5858f2c377bbd32f350c22f4a3964f39ee7b
Alexis <pingrisalexis@gmail.com>
2021-11-11 11:16:24 +0100

    capture focus contact form / lightbox

---
+++ src/js/focusableElements.js
@@ -0,0 +1,38 @@
+//LISTEN TO THE TRAP FOCUS TAB
+document.addEventListener("keydown", function(e) {
+    let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+    if (!isTabPressed) {
+        return;
+    }
+
+    if (e.shiftKey) {
+
+        if (document.activeElement === firstFocusableElementLightbox) {
+            lastFocusableElementLightbox.focus();
+            e.preventDefault();
+            console.log("focus lightbox last element")
+            console.log(firstFocusableElementLightbox)
+            console.log(lastFocusableElementLightbox)
+        }
+            else  if (document.activeElement === firstFocusableElementFormContact) {
+                lastFocusableElementFormContact.focus();
+                e.preventDefault();
+                console.log("focus form contact last element")
+            }
+
+    } else {
+        if (document.activeElement === lastFocusableElementLightbox) {
+            e.preventDefault();
+            firstFocusableElementLightbox.focus();
+
+            console.log("focus lightbox first element")
+        }
+            else   if (document.activeElement === lastFocusableElementFormContact ) {
+                e.preventDefault();
+                firstFocusableElementFormContact.focus();
+                console.log("focus form contact  first element")
+            }
+
+    }
+});
